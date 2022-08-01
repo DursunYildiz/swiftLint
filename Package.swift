ae4c2b1f0843508a69c99295d6e3efa00669e0b2
Dursun Yıldız <55432831+DursunYildiz@users.noreply.github.com>
2022-08-01 14:11:07 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftLintLibrary",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
+        .plugin(
             name: "SwiftLintLibrary",
             targets: ["SwiftLintLibrary"]),
     ],
