75176760bd1fa00cfca13253181503962c767fd7
Dursun Yıldız <55432831+DursunYildiz@users.noreply.github.com>
2022-08-02 10:55:45 +0300

    Update SwiftLintPlugin.swift

--- Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
+++ Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
@@ -23,18 +23,28 @@ struct SwiftLintPlugin: BuildToolPlugin {
             return []
         }
         return [
+           
+import PackagePlugin
+
+@main
+struct SwiftLintPlugin: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        return [
             .buildCommand(
-                displayName: "Running SwiftLint for \(target.name)",
+                displayName: "Run SwiftLint for \(target.name)",
                 executable: try context.tool(named: "swiftlint").path,
                 arguments: [
                     "lint",
                     "--in-process-sourcekit",
                     "--path",
-                    target.directory.string,
-                    "--config",
-                    "\(context.package.directory.string)/.swiftlint.yml"
+                    target.directory.string
                 ],
                 environment: [:]
+            )
+        ]
+    }
+}
+
             )
         ]
     }
